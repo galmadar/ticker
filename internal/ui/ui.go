28a5b0a4f70921c6d041506357e217a8ad322373
Gal Madar <madargal@gmail.com>
2021-02-04 16:53:02 +0200

    style: added text

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -132,7 +132,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	if !m.ready {
-		return "\n  Initalizing..."
+		return "\n  HI! Initalizing..."
 	}
 
 	return strings.Join(
